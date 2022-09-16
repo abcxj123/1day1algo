888e7dacb2c4de03cb2215719cd96afe502dcca1
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2022-09-16 19:56:09 +0900

    Create BOJ_B5_2753_윤년.java

---
+++ Algo_2022_09/BOJ_B5_2753_윤년.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BOJ_B5_2753_윤년 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int year = scann.nextInt();
+		
+		if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
+		}
+
+	}
+
+}
