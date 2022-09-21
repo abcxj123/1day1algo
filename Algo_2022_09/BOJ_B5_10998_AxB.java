3ba050d34d68cee9a87791d1c655cafd163c9b12
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2022-09-21 23:38:51 +0900

    Create BOJ_B5_10998_AxB.java

---
+++ Algo_2022_09/BOJ_B5_10998_AxB.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B5_10998_AxB {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int A = scann.nextInt();
+		int B = scann.nextInt();
+		
+		System.out.println(A*B);
+
+	}
+
+}
