1ae878f40fe8d9feeaa11c19c9bfc349c2997497
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2022-09-30 00:04:02 +0900

    Create BOJ_B5_10869_사칙연산.java

---
+++ BOJ_B5_10869_사칙연산.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BOJ_B5_10869_사칙연산 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int A = scann.nextInt();
+		int B = scann.nextInt();
+		
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+
+	}
+
+}
