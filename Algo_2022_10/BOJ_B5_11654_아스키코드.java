846a31547a9a2be917bcf8a734a128b51509c7fa
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2022-10-23 10:55:30 +0900

    Create BOJ_B5_11654_아스키코드.java

---
+++ Algo_2022_10/BOJ_B5_11654_아스키코드.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BOJ_B5_11654_아스키코드 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		char c = scann.next().charAt(0);
+		
+		int ans = (int) c;
+		
+		System.out.println(ans);
+
+	}
+
+}
