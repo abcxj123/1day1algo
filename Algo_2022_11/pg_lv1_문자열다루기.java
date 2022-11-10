15a975714612152a2e4ffe1ccb613c3773aed784
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2022-11-11 08:47:25 +0900

    Create pg_lv1_문자열다루기.java

---
+++ Algo_2022_11/pg_lv1_문자열다루기.java
@@ -0,0 +1,14 @@
+class pg_lv1_문자열다루기 {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if(s.length() != 4 && s.length() != 6) return false;
+        for(int i=0;i<s.length();i++) {
+            char c = s.charAt(i);
+            if(c < '0' || c > '9') {
+                answer = false;
+                break;
+            }
+        }
+        return answer;
+    }
+}
