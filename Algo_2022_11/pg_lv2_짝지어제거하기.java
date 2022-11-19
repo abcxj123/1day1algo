f24c2209d8a8bd0cdbd8891a4cbae7bc0dc13ccb
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2022-11-19 11:45:53 +0900

    Create pg_lv2_짝지어제거하기.java

---
+++ Algo_2022_11/pg_lv2_짝지어제거하기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class pg_lv2_짝지어제거하기
+{
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0;i<s.length();i++) {
+            char c = s.charAt(i);
+            if(!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        if(stack.isEmpty()) return 1;
+        else return 0;
+    }
+}
