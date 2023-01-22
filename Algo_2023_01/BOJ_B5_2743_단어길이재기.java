ae4decf29376d90da80206f6fd4363144277cfce
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-01-22 19:09:55 +0900

    Create BOJ_B5_2743_단어길이재기.java

---
+++ Algo_2023_01/BOJ_B5_2743_단어길이재기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B5_2743_단어길이재기 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		String str = scann.next();
+		
+		System.out.println(str.length());
+
+	}
+
+}
