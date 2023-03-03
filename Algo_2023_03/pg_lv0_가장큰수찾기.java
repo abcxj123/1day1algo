431ecc3de6715204333161264a02f15678153a23
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-03-03 23:18:44 +0900

    Create pg_lv0_가장큰수찾기.java

---
+++ Algo_2023_03/pg_lv0_가장큰수찾기.java
@@ -0,0 +1,15 @@
+class pg_lv0_가장큰수찾기 {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int max = -1;
+        int idx = -1;
+        for(int i=0;i<array.length;i++) {
+            if(array[i] > max) {
+                max = array[i];
+                answer[0] = max;
+                answer[1] = i;
+            }
+        }
+        return answer;
+    }
+}
