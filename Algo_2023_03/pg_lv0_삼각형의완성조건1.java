eff012cd850004040245826979ad5e9a8883590f
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-03-12 01:35:33 +0900

    Create pg_lv0_삼각형의완성조건1.java

---
+++ Algo_2023_03/pg_lv0_삼각형의완성조건1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class pg_lv0_삼각형의완성조건1 {
+    public int solution(int[] sides) {
+        int answer = 0;
+        Arrays.sort(sides);
+        if(sides[0]+sides[1] > sides[2]) {
+            answer = 1;
+        } else {
+            answer = 2;
+        }
+        
+        return answer;
+    }
+}
