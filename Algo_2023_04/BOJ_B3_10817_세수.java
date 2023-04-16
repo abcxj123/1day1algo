f0420370a155f31f0e7a8a9e5e2c6a460eeee29d
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-04-16 16:48:32 +0900

    Create BOJ_B3_10817_세수.java

---
+++ Algo_2023_04/BOJ_B3_10817_세수.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ_B3_10817_세수 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int [] arr = new int[3];
+		
+		for (int i = 0; i < 3; i++) {
+			arr[i] = scann.nextInt();
+		}
+
+		Arrays.sort(arr);
+		
+		System.out.println(arr[1]);
+		
+	}
+
+}
