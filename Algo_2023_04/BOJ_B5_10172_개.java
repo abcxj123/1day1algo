451c3b29a18f55f5eaa1a63aa1b305c298f3f4cb
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-04-30 21:45:57 +0900

    Create BOJ_B5_10172_개.java

---
+++ Algo_2023_04/BOJ_B5_10172_개.java
@@ -0,0 +1,17 @@
+
+public class BOJ_B5_10172_개 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("|\\_/|").append("\n");
+		sb.append("|q p|   /}").append("\n");
+		sb.append("( 0 )\"\"\"\\").append("\n");
+		sb.append("|\"^\"`    |").append("\n");
+		sb.append("||_/=\\\\__|");
+		
+		System.out.println(sb.toString());
+
+	}
+
+}
