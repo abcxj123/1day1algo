06b66fd79e7c7601538ac86f3da79659f593bae4
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-05-07 19:37:03 +0900

    Create BOJ_B5_11382_꼬마정민.java

---
+++ Algo_2023_05/BOJ_B5_11382_꼬마정민.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BOJ_B5_11382_꼬마정민 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		long a = scann.nextLong();
+		long b = scann.nextLong();
+		long c = scann.nextLong();
+		
+		System.out.println(a+b+c);
+				
+	}
+
+}
