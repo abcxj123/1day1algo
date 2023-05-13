89e77d81aed2032a0c7bc39ed89bf34fc16f6c55
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-05-13 12:58:54 +0900

    Create BOJ_S3_11659_구간합구하기4.java

---
+++ Algo_2023_05/BOJ_S3_11659_구간합구하기4.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_S3_11659_구간합구하기4 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int [] arr = new int[N+1];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			sb.append(arr[end]-arr[start-1]).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+
+	}
+
+}
