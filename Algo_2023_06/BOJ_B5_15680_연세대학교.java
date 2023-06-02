5b6a2952ae145fd04e8c35624950110fa14f144e
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-06-02 22:22:10 +0900

    Create BOJ_B5_15680_연세대학교.java

---
+++ Algo_2023_06/BOJ_B5_15680_연세대학교.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BOJ_B5_15680_연세대학교 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		
+		if(N == 0) {
+			System.out.println("YONSEI");
+		} else if(N == 1) {
+			System.out.println("Leading the Way to the Future");
+		}
+
+	}
+
+}
