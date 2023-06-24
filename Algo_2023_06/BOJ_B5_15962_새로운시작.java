d01922fda914bed170b83f80090f41bb7f7e9bad
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-06-24 23:30:33 +0900

    Create BOJ_B5_15962_새로운시작.java

---
+++ Algo_2023_06/BOJ_B5_15962_새로운시작.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BOJ_B5_15962_새로운시작 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		System.out.println("파이팅!!");
+
+	}
+
+}
