a696cc49a8ef8d5c6df61fe8b1637472554f7a0d
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-06-28 23:53:57 +0900

    Create BOJ_B4_9316_HelloJudge.java

---
+++ Algo_2023_06/BOJ_B4_9316_HelloJudge.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B4_9316_HelloJudge {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		int N = scann.nextInt();
+		
+		for (int i = 1; i <= N; i++) {
+			System.out.println("Hello World, Judge "+i+"!");
+		}
+
+	}
+
+}
