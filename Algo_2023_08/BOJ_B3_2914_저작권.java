b46411eea3185f87cc52f8a0f21e14c34cc53ddf
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-08-12 14:39:56 +0900

    Create BOJ_B3_2914_저작권.java

---
+++ Algo_2023_08/BOJ_B3_2914_저작권.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B3_2914_저작권 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int A = scann.nextInt();
+		int I = scann.nextInt();
+		
+		System.out.println(A*I-(A-1));
+
+	}
+
+}
