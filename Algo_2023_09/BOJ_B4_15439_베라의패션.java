f73b1e630841becf03d47a85bba9730f3f58a601
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-09-17 13:31:13 +0900

    Create BOJ_B4_15439_베라의패션.java

---
+++ Algo_2023_09/BOJ_B4_15439_베라의패션.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B4_15439_베라의패션 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		
+		System.out.println(N * (N-1));
+
+	}
+
+}
