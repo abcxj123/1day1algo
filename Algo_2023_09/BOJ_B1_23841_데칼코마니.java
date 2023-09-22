5d7687097bebd822d3b79df39d1bf6722006d25e
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-09-22 21:05:02 +0900

    Create BOJ_B1_23841_데칼코마니.java

---
+++ Algo_2023_09/BOJ_B1_23841_데칼코마니.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_B1_23841_데칼코마니 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		char [][] arr = new char[N][M];
+		
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = str.charAt(j);
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				char c = arr[i][j];
+				if(c != '.') {
+					arr[i][M-1-j] = c;
+				}
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+}
