a132b8710fc7f5919e7dbb376217cd590dd3eed7
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-10-04 17:00:32 +0900

    Create BOJ_S5_16212_정열적인정렬.java

---
+++ Algo_2023_10/BOJ_S5_16212_정열적인정렬.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_S5_16212_정열적인정렬 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int [] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int n : arr) {
+			sb.append(n+" ");
+		}
+		
+		sb.setLength(sb.length()-1);
+		System.out.println(sb.toString());
+		
+	}
+
+}
