09a2b28f3e143d62906339cf27d4d017e54d34bf
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-11-12 23:17:37 +0900

    Create BOJ_B4_24723_녹색거탑.java

---
+++ Algo_2023_11/BOJ_B4_24723_녹색거탑.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B4_24723_녹색거탑 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		N = (int) Math.pow(2, N);
+		
+		System.out.println(N);
+
+	}
+
+}
