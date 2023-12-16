03b01273a13e94239d1aca71c1341a78cb1cdbb0
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-12-16 17:25:25 +0900

    Create BOJ_B3_15894_수학은체육과목입니다.java

---
+++ Algo_2023_12/BOJ_B3_15894_수학은체육과목입니다.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BOJ_B3_15894_수학은체육과목입니다 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		long N = scann.nextLong();
+
+		System.out.println(N*4);
+	}
+
+}
