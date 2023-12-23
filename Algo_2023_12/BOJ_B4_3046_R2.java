0be778e8470513a5dea39ace0e197fdf1e14ade8
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2023-12-23 17:36:28 +0900

    Create BOJ_B4_3046_R2.java

---
+++ Algo_2023_12/BOJ_B4_3046_R2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B4_3046_R2 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int R1 = scann.nextInt();
+		int S = scann.nextInt();
+		
+		System.out.println(S*2-R1);
+
+	}
+
+}
