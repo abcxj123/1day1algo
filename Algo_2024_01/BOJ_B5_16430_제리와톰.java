f85c305ac370351409c4f66f9dea91dc64be279a
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-01-20 15:20:38 +0900

    Create BOJ_B5_16430_제리와톰.java

---
+++ Algo_2024_01/BOJ_B5_16430_제리와톰.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B5_16430_제리와톰 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int A = scann.nextInt();
+		int B = scann.nextInt();
+		
+		System.out.println((B-A)+" "+B);
+
+	}
+
+}
