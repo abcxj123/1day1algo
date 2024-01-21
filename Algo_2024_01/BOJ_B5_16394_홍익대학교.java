53fb80ea874b6e9ab769532b79a745a44363d498
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-01-21 20:00:11 +0900

    Create BOJ_B5_16394_홍익대학교.java

---
+++ Algo_2024_01/BOJ_B5_16394_홍익대학교.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B5_16394_홍익대학교 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		
+		System.out.println(N-1946);
+
+	}
+
+}
