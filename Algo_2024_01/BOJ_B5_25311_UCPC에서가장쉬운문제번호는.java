0f283afe6ba461294aab4b7dc085046cc1ce9d81
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-01-26 21:29:14 +0900

    Create BOJ_B5_25311_UCPC에서가장쉬운문제번호는.java

---
+++ Algo_2024_01/BOJ_B5_25311_UCPC에서가장쉬운문제번호는.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B5_25311_UCPC에서가장쉬운문제번호는 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		
+		System.out.println('A');
+
+	}
+
+}
