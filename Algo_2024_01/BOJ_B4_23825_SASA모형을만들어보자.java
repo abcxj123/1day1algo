04b00172c79681f04e061d33b189c26cffd57023
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-01-27 15:40:47 +0900

    Create BOJ_B4_23825_SASA모형을만들어보자.java

---
+++ Algo_2024_01/BOJ_B4_23825_SASA모형을만들어보자.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B4_23825_SASA모형을만들어보자 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt()/2;
+		int M = scann.nextInt()/2;
+		
+		System.out.println(Math.min(N, M));
+
+	}
+
+}
