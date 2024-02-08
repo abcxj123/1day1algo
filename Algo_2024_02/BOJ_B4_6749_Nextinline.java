6a33909a269117a897a0734b5efdc87c4881959a
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-02-08 22:55:15 +0900

    Create BOJ_B4_6749_Nextinline.java

---
+++ Algo_2024_02/BOJ_B4_6749_Nextinline.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B4_6749_Nextinline {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int a = scann.nextInt();
+		int b = scann.nextInt();
+		System.out.println(b + (b-a));
+
+	}
+
+}
