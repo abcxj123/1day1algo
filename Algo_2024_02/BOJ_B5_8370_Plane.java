b8907cc2f1856adfe405ad355ec488cc54a8d4f7
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-02-10 15:49:08 +0900

    Create BOJ_B5_8370_Plane.java

---
+++ Algo_2024_02/BOJ_B5_8370_Plane.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BOJ_B5_8370_Plane {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int n1 = scann.nextInt();
+		int k1 = scann.nextInt();
+		int n2 = scann.nextInt();
+		int k2 = scann.nextInt();
+		
+		System.out.println(n1*k1 + n2*k2);
+
+	}
+
+}
