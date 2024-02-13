dabe52e7a13ac68e05653832c8650d7c30ef7143
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-02-13 10:24:50 +0900

    Create BOJ_B3_2441_별찍기4.java

---
+++ Algo_2024_02/BOJ_B3_2441_별찍기4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BOJ_B3_2441_별찍기4 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int N = scann.nextInt();
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < i; j++) {
+				sb.append(' ');
+			}
+			for (int j = i; j < N; j++) {
+				sb.append('*');
+			}
+			sb.append('\n');
+		}
+		
+		sb.setLength(sb.length()-1);
+		System.out.println(sb.toString());
+
+	}
+
+}
