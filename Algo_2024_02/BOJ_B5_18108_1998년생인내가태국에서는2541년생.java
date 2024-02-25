17b802f5a7470d9214c6078d8e5a83543b99424e
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-02-25 16:41:14 +0900

    Create BOJ_B5_18108_1998년생인내가태국에서는2541년생.java

---
+++ Algo_2024_02/BOJ_B5_18108_1998년생인내가태국에서는2541년생.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B5_18108_1998년생인내가태국에서는2541년생 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int y = scann.nextInt();
+		
+		System.out.println(y-543);
+
+	}
+
+}
