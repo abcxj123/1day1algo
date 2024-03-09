0f1b3954be16a30be64b0add577c4393ddb8a36b
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-03-09 15:05:36 +0900

    Create BOJ_B5_5338_마이크로소프트로고.java

---
+++ Algo_2024_03/BOJ_B5_5338_마이크로소프트로고.java
@@ -0,0 +1,12 @@
+
+public class BOJ_B5_5338_마이크로소프트로고 {
+
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._\n"
+				+ "'-..-'|   ||   |\n"
+				+ "'-..-'|_.-;;-._|\n"
+				+ "'-..-'|   ||   |\n"
+				+ "'-..-'|_.-''-._|");
+	}
+
+}
