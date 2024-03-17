1d75ede5b4d20eb6ec1a0b1412e1e0cfe2a0bf46
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-03-17 13:44:22 +0900

    Create BOJ_B5_2558_AB2.java

---
+++ Algo_2024_03/BOJ_B5_2558_AB2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B5_2558_AB2 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int A = scann.nextInt();
+		int B = scann.nextInt();
+		
+		System.out.println(A+B);
+
+	}
+
+}
