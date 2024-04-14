5e17657e08c7c344fb399983b05a7cba38ac3ce9
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-04-14 19:15:36 +0900

    Create BOJ_B5_5337_웰컴.java

---
+++ Algo_2024_04/BOJ_B5_5337_웰컴.java
@@ -0,0 +1,11 @@
+
+public class BOJ_B5_5337_웰컴 {
+
+	public static void main(String[] args) {
+		System.out.println(".  .   .\n"
+				+ "|  | _ | _. _ ._ _  _\n"
+				+ "|/\\|(/.|(_.(_)[ | )(/.");
+
+	}
+
+}
