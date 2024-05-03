ae3824cc50939af78a2410c25ff159052867f33e
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-05-03 21:28:08 +0900

    Create BOJ_B5_30030_스위트콘가격구하기.java

---
+++ Algo_2024_05/BOJ_B5_30030_스위트콘가격구하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B5_30030_스위트콘가격구하기 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int B = scann.nextInt();
+		
+		System.out.println(B/11*10);
+
+	}
+
+}
