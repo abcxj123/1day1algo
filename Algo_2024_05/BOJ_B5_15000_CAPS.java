8cb63d65b94644b0003715c3ed0fb199cb2708af
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-05-20 20:53:43 +0900

    Create BOJ_B5_15000_CAPS.java

---
+++ Algo_2024_05/BOJ_B5_15000_CAPS.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B5_15000_CAPS {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		String str = scann.nextLine();
+		
+		System.out.println(str.toUpperCase());
+
+	}
+
+}
