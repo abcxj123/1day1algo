d00f8e1635ba909724d5102c725d645cb8020a54
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-05-21 09:05:18 +0900

    Create BOJ_B5_24078_Remainder.java

---
+++ Algo_2024_05/BOJ_B5_24078_Remainder.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B5_24078_Remainder {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int x = scann.nextInt();
+		
+		System.out.println(x%21);
+
+	}
+
+}
