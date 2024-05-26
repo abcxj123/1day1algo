0746c12ed7bd8912375cab762691734f6cd023c5
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-05-26 21:33:35 +0900

    Create BOJ_B4_24082_Cube.java

---
+++ Algo_2024_05/BOJ_B4_24082_Cube.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B4_24082_Cube {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int x = scann.nextInt();
+		
+		System.out.println(x*x*x);
+
+	}
+
+}
