1149b298ea01a8b8d0867e66f04563f6cdeefb7d
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-06-21 17:46:31 +0900

    Create BOJ_B5_10189_Hook.java

---
+++ Algo_2024_06/BOJ_B5_10189_Hook.java
@@ -0,0 +1,12 @@
+
+public class BOJ_B5_10189_Hook {
+
+	public static void main(String[] args) {
+		System.out.println("#  # #### #### #  #\n"
+				+ "#### #  # #  # # #\n"
+				+ "#### #  # #  # # #\n"
+				+ "#  # #### #### #  #");
+
+	}
+
+}
