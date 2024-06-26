d14fcce9793033084ff09b1e9db97cef9f55b16e
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-06-26 09:54:32 +0900

    Create BOJ_B4_31668_특별한가지.java

---
+++ Algo_2024_06/BOJ_B4_31668_특별한가지.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BOJ_B4_31668_특별한가지 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		int M = scann.nextInt();
+		int K = scann.nextInt();
+		
+		System.out.println((M/N)*K);
+
+	}
+
+}
