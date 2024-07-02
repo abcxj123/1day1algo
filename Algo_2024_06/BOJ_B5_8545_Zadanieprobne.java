023412c7214b4a2a023ce69f7af756cb714c41d7
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-07-02 09:01:53 +0900

    Create BOJ_B5_8545_Zadanieprobne.java

---
+++ Algo_2024_06/BOJ_B5_8545_Zadanieprobne.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BOJ_B5_8545_Zadanieprobne {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		String str = scann.nextLine();
+		
+		sb.append(str);
+		
+		System.out.println(sb.reverse().toString());
+
+	}
+
+}
