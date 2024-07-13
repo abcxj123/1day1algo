43b80fd123a85c9d527f810be9bf61eb7e7180d2
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-07-13 16:51:34 +0900

    Create BOJ_B5_31606_Fruit.java

---
+++ Algo_2024_07/BOJ_B5_31606_Fruit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B5_31606_Fruit {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int X = scann.nextInt();
+		int Y = scann.nextInt();
+		
+		System.out.println(X+Y+3);
+
+	}
+
+}
