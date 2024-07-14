4ce3051be408f6524b4251c3883137ede78f719f
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-07-14 22:13:48 +0900

    Create BOJ_B5_31610_Drops_Packing.java

---
+++ Algo_2024_07/BOJ_B5_31610_Drops_Packing.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BOJ_B5_31610_Drops_Packing {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int A = scann.nextInt();
+		int B = scann.nextInt();
+		int C = scann.nextInt();
+		
+		System.out.println(A*B+C);
+
+	}
+
+}
