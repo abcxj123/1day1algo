cac329c69fe16bcee254439bf19e32fd3a9d616f
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-07-27 10:15:31 +0900

    Create BOJ_B4_31615_Digit.java

---
+++ Algo_2024_07/BOJ_B4_31615_Digit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B4_31615_Digit {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int A = scann.nextInt();
+		int B = scann.nextInt();
+		
+		System.out.println(String.valueOf(A+B).length());
+
+	}
+
+}
