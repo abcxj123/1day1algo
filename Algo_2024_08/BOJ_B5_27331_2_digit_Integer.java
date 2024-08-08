a051f93f05e27c528f543fb5fb3b895b42ed9561
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-08-08 23:25:43 +0900

    Create BOJ_B5_27331_2_digit_Integer.java

---
+++ Algo_2024_08/BOJ_B5_27331_2_digit_Integer.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B5_27331_2_digit_Integer {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int A = scann.nextInt();
+		int B = scann.nextInt();
+		
+		System.out.println(A*10+B);
+
+	}
+
+}
