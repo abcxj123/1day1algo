c693359b46d546aa7c6809afc2374dc03c2d0716
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-08-16 22:33:18 +0900

    Create BOJ_B5_27327_Hour.java

---
+++ Algo_2024_08/BOJ_B5_27327_Hour.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B5_27327_Hour {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int x = scann.nextInt();
+		
+		System.out.println(x*24);
+
+	}
+
+}
