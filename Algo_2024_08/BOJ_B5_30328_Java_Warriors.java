9f95f7e51149cf9a8e193cfe6aa44cff6728c3f8
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-08-18 20:48:02 +0900

    Create BOJ_B5_30328_Java_Warriors.java

---
+++ Algo_2024_08/BOJ_B5_30328_Java_Warriors.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B5_30328_Java_Warriors {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		
+		System.out.println(N*4000);
+
+	}
+
+}
