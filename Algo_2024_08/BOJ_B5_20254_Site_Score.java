350ba180abf0abbbbfc3b2deffbeaf664f79fe47
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-08-27 23:31:02 +0900

    Create BOJ_B5_20254_Site_Score.java

---
+++ Algo_2024_08/BOJ_B5_20254_Site_Score.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BOJ_B5_20254_Site_Score {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int a = scann.nextInt();
+		int b = scann.nextInt();
+		int c = scann.nextInt();
+		int d = scann.nextInt();
+		
+		System.out.println(56*a + 24*b + 14*c + 6*d);
+
+	}
+
+}
