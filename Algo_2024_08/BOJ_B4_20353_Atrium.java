43ef35b533766bbc322ef2d32bf4a1985b51d74e
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-08-31 11:40:08 +0900

    Create BOJ_B4_20353_Atrium.java

---
+++ Algo_2024_08/BOJ_B4_20353_Atrium.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B4_20353_Atrium {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		long n = scann.nextLong();
+		
+		System.out.println(Math.sqrt(n)*4);
+
+	}
+
+}
