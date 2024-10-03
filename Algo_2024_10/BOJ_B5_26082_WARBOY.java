323b74d0142edac48f7167e7bf143c0d5eae1cd2
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-10-03 20:18:31 +0900

    Create BOJ_B5_26082_WARBOY.java

---
+++ Algo_2024_10/BOJ_B5_26082_WARBOY.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BOJ_B5_26082_WARBOY {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int A = scann.nextInt();
+		int B = scann.nextInt();
+		int C = scann.nextInt();
+		
+		System.out.println(B/A*3*C);
+
+	}
+
+}
