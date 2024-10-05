ca1ff8317599c83097c888541c151cf0a899461c
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-10-05 22:04:53 +0900

    Create BOJ_B4_13597_Tridu.java

---
+++ Algo_2024_10/BOJ_B4_13597_Tridu.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B4_13597_Tridu {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int a = scann.nextInt();
+		int b = scann.nextInt();
+		
+		System.out.println(Math.max(a, b));
+
+	}
+
+}
