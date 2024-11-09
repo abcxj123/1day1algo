8923711d90acf4965f343d6365b743d646332e2e
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-11-09 23:49:57 +0900

    Create BOJ_B5_31614_Minutes.java

---
+++ Algo_2024_11/BOJ_B5_31614_Minutes.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B5_31614_Minutes {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int h = scann.nextInt();
+		int m = scann.nextInt();
+		
+		System.out.println(h*60+m);
+
+	}
+
+}
