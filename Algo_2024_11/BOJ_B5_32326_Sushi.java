c10eb5433ec4a4c3fb2c12fcd123ce8bcb62d3c2
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-11-10 22:12:14 +0900

    Create BOJ_B5_32326_Sushi.java

---
+++ Algo_2024_11/BOJ_B5_32326_Sushi.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BOJ_B5_32326_Sushi {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int a = scann.nextInt();
+		int b = scann.nextInt();
+		int c = scann.nextInt();
+		
+		System.out.println(a*3+b*4+c*5);
+
+	}
+
+}
