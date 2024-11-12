c51556dc78982b0f70e65537fbbd68ff82aebf33
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-11-12 23:00:43 +0900

    Create BOJ_B5_21598_SciComLove.java

---
+++ Algo_2024_11/BOJ_B5_21598_SciComLove.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BOJ_B5_21598_SciComLove {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		
+		for (int i = 0; i < N; i++) {
+			System.out.println("SciComLove");
+		}
+
+	}
+
+}
