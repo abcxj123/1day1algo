25c20d43ed60991ab6a956751e4611d74796319b
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-12-13 15:20:20 +0900

    Create BOJ_B4_32215_코드마스터2024.java

---
+++ Algo_2024_12/BOJ_B4_32215_코드마스터2024.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BOJ_B4_32215_코드마스터2024 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		int M = scann.nextInt();
+		int K = scann.nextInt();
+		
+		System.out.println((K+1)*M);
+
+	}
+
+}
