747a7e1b6fc9d43f0d57af36a52d364a6e09ab68
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2024-12-21 15:59:48 +0900

    Create BOJ_B4_16600_Contemporary_Art.java

---
+++ Algo_2024_12/BOJ_B4_16600_Contemporary_Art.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B4_16600_Contemporary_Art {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		long N = scann.nextLong();
+		
+		System.out.println(Math.sqrt(N)*4);
+
+	}
+
+}
