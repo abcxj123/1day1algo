0ddafbcc2289a6111bc94e4b9e4e0405a582e710
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2025-01-12 23:38:10 +0900

    Create BOJ_B5_32951_AI선도대학.java

---
+++ Algo_2025_01/BOJ_B5_32951_AI선도대학.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BOJ_B5_32951_AI선도대학 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		
+		System.out.println(N-2024);
+	}
+
+}
