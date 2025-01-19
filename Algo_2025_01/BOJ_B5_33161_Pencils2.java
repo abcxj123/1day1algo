5f52a7874cfe6fa62316b47f41b62857028c1899
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2025-01-19 23:51:29 +0900

    Create BOJ_B5_33161_Pencils2.java

---
+++ Algo_2025_01/BOJ_B5_33161_Pencils2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B5_33161_Pencils2 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		
+		System.out.println(N/5);
+
+	}
+
+}
