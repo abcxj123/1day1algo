e0fc7a4c6bde69149186bcf735cdfc550489f303
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2025-01-31 19:51:16 +0900

    Create BOJ_B4_32902_Chips.java

---
+++ Algo_2025_01/BOJ_B4_32902_Chips.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B4_32902_Chips {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int a = scann.nextInt();
+		int b = scann.nextInt();
+		
+		System.out.println((b+1)+" "+(a*b+1));
+
+	}
+
+}
