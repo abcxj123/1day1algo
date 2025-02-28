8426b826008a625b4993dbcb75b86320b689f934
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2025-02-28 23:54:31 +0900

    Create BOJ_B5_32458_Just_Round_Down.java

---
+++ Algo_2025_02/BOJ_B5_32458_Just_Round_Down.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B5_32458_Just_Round_Down {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		double d = scann.nextDouble();
+		
+		System.out.println((int)d);
+
+	}
+
+}
