25b1434561d6c24f0ddee19ab1c5f7f336f9982b
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2025-03-29 08:55:12 +0900

    Create BOJ_B2_10821_정수의개수.java

---
+++ Algo_2025_03/BOJ_B2_10821_정수의개수.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B2_10821_정수의개수 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		String s = scann.nextLine();
+		String [] arr = s.split(",");
+		
+		System.out.println(arr.length);
+
+	}
+
+}
