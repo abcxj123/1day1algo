3f99fe135e0782b4a70994d9ad7d18c3e74b2936
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2025-04-01 23:47:39 +0900

    Create BOJ_B1_30455_이제는더이상물러날곳이없다.java

---
+++ Algo_2025_04/BOJ_B1_30455_이제는더이상물러날곳이없다.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B1_30455_이제는더이상물러날곳이없다 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		
+		if(N % 2 == 0) System.out.println("Duck");
+		else System.out.println("Goose");
+
+	}
+
+}
