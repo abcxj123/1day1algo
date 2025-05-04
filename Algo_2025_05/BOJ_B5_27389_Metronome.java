17f8dda8ab4531ed680aa93ebefc48575152a290
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2025-05-04 23:28:56 +0900

    Create BOJ_B5_27389_Metronome.java

---
+++ Algo_2025_05/BOJ_B5_27389_Metronome.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B5_27389_Metronome {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		
+		System.out.println(N / 4.0);
+
+	}
+
+}
