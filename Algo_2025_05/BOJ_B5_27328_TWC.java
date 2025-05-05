c84b95f13d9adc9a23140d1150665631b9f112c1
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2025-05-05 23:55:25 +0900

    Create BOJ_B5_27328_TWC.java

---
+++ Algo_2025_05/BOJ_B5_27328_TWC.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B5_27328_TWC {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int A = scann.nextInt();
+		int B = scann.nextInt();
+		
+		System.out.println(Integer.compare(A, B));
+
+	}
+
+}
