352b23b123f9448264145485bed3acd1270644cf
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2025-06-11 15:43:39 +0900

    Create BOJ_B5_18096_러시아.java

---
+++ Algo_2025_06/BOJ_B5_18096_러시아.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B5_18096_러시아 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		scann.nextInt();
+		
+		System.out.println(1);
+
+	}
+
+}
