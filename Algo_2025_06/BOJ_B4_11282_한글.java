fafc2dd245d8b33c95af70f16a9b7c50f3465881
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2025-06-12 21:59:50 +0900

    Create BOJ_B4_11282_한글.java

---
+++ Algo_2025_06/BOJ_B4_11282_한글.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B4_11282_한글 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		N += 44031;
+		
+		System.out.println((char)N);
+
+	}
+
+}
