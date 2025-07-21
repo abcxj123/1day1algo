e30c6c80040854956ffd71d3b6f74b0e1108b395
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2025-07-21 23:57:21 +0900

    Create BOJ_B5_10757_큰수AB.java

---
+++ Algo_2025_07/java/BOJ_B5_10757_큰수AB.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BOJ_B5_10757_큰수AB {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		BigInteger A = new BigInteger(sc.next());  
+		BigInteger B = new BigInteger(sc.next());  
+		   
+		System.out.println(A.add(B));
+
+	}
+
+}
