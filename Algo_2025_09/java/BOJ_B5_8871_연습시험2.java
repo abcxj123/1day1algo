ca9cbabd2d85373982427abafc02ced74dea5742
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2025-09-08 22:23:10 +0900

    Create BOJ_B5_8871_연습시험2.java

---
+++ Algo_2025_09/java/BOJ_B5_8871_연습시험2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BOJ_B5_8871_연습시험2 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		
+		int a = (N+1)*2;
+		int b = (N+1)*3;
+		
+		System.out.println(a+" "+b);
+
+	}
+
+}
