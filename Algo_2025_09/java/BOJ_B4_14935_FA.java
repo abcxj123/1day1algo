995786e90dc19a7979c9092d210557e25ff16a5d
abcxj123 <99263360+abcxj123@users.noreply.github.com>
2025-09-09 23:31:25 +0900

    Create BOJ_B4_14935_FA.java

---
+++ Algo_2025_09/java/BOJ_B4_14935_FA.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B4_14935_FA {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		scann.nextLine();
+		
+		System.out.println("FA");
+
+	}
+
+}
