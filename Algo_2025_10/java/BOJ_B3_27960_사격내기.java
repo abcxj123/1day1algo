8fbb6ef5aa11ec119f4f68cf4e7e1e4efe48d48d
정건우 <abcxj123@naver.com>
2025-10-24 22:46:00 +0900

    Create BOJ_B3_27960_사격내기.java

---
+++ Algo_2025_10/java/BOJ_B3_27960_사격내기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B3_27960_사격내기 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int a = scann.nextInt();
+		int b = scann.nextInt();
+		
+		System.out.println(a^b);
+
+	}
+
+}
