ea56079ee5fed86fc18d1fe53a48034cb7787c9b
정건우 <abcxj123@naver.com>
2025-10-27 21:38:04 +0900

    Create BOJ_B2_24267_알고리즘의수행시간6.java

---
+++ Algo_2025_10/java/BOJ_B2_24267_알고리즘의수행시간6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B2_24267_알고리즘의수행시간6 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		long N = scann.nextLong();
+		
+		System.out.println((N*(N-1)*(N-2)/6));
+		System.out.println(3);
+
+	}
+
+}
