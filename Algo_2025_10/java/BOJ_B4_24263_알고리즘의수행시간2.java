b854d42e5089da5e4e6b82dad0fcd62c975763db
정건우 <abcxj123@naver.com>
2025-10-29 23:34:08 +0900

    Create BOJ_B4_24263_알고리즘의수행시간2.java

---
+++ Algo_2025_10/java/BOJ_B4_24263_알고리즘의수행시간2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B4_24263_알고리즘의수행시간2 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		long N = scann.nextLong();
+		
+		System.out.println(N);
+		System.out.println(1);
+
+	}
+
+}
