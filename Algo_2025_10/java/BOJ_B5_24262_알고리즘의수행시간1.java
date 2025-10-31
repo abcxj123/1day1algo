d32c99dab6b7074e3bcb9fc4f7e20bfc2b8b41e7
정건우 <abcxj123@naver.com>
2025-10-31 23:13:02 +0900

    Create BOJ_B5_24262_알고리즘의수행시간1.java

---
+++ Algo_2025_10/java/BOJ_B5_24262_알고리즘의수행시간1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B5_24262_알고리즘의수행시간1 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		
+		System.out.println(1);
+		System.out.println(0);
+
+	}
+
+}
