e8717484bc13e64648dda507acb784217a102b1d
정건우 <abcxj123@naver.com>
2025-11-21 22:16:50 +0900

    Create BOJ_B1_25400_제자리.java

---
+++ Algo_2025_11/BOJ_B1_25400_제자리.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_B1_25400_제자리 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int ans = 0;
+		int next = 1;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int n = Integer.parseInt(st.nextToken());
+			
+			if(n == next) {
+				next++;
+			} else {
+				ans++;
+			}
+		}
+		
+		System.out.println(ans);
+		
+	}
+
+}
