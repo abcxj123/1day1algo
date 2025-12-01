6d269e421f14657c864155d3d994b89e34c9aa2d
정건우 <abcxj123@naver.com>
2025-12-01 21:08:47 +0900

    Create BOJ_B3_2446_별찍기9.java

---
+++ Algo_2025_11/BOJ_B3_2446_별찍기9.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_B3_2446_별찍기9 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i = N; i >= 1; i--) {
+            for (int j = 0; j < N-i; j++) {
+                sb.append(" ");
+            }
+            
+            for (int j = 1; j <= 2*i-1; j++) {
+                sb.append("*");
+            }
+            
+            sb.append("\n");
+        }
+        
+        for (int i = 2; i <= N; i++) {
+            for (int j = N-i; j >= 1; j--) {
+                sb.append(" ");
+            }
+            
+            for (int j = 1; j <= 2*i-1; j++) {
+                sb.append("*");
+            }
+            
+            sb.append("\n");
+            
+        }
+        
+        System.out.print(sb.toString());
+        
+    }
+
+}
