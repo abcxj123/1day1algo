2c090d1e1663d9e930f93942a7efb85006c30bd7
정건우 <abcxj123@naver.com>
2025-12-08 22:56:03 +0900

    Create BOJ_B1_2163_초콜릿자르기.java

---
+++ Algo_2025_12/BOJ_B1_2163_초콜릿자르기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_B1_2163_초콜릿자르기 {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		int M = scann.nextInt();
+		
+		System.out.println(N*M-1);
+
+	}
+
+}
