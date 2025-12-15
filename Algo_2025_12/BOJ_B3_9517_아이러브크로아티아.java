c7863e4dc662928d55d4bc7c5c05df23aee81cd9
정건우 <abcxj123@naver.com>
2025-12-15 22:01:55 +0900

    Create BOJ_B3_9517_아이러브크로아티아.java

---
+++ Algo_2025_12/BOJ_B3_9517_아이러브크로아티아.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_B3_9517_아이러브크로아티아 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int K = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		int time = 0;
+		
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int T = Integer.parseInt(st.nextToken());
+			String Z = st.nextToken();
+			
+			time += T;
+			
+			if(time > 210) break;
+			
+			if(Z.equals("T")) K++;
+			
+			if(K == 9) K = 1;
+		}
+		
+		System.out.println(K);
+		
+	}
+
+}
